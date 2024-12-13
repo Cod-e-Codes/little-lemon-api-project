753ab4f1e45c017a4e479619edacad7c1f199831
Cody Marsengill <codylmarsengill@gmail.com>
2024-12-13 17:58:19 -0500

    setup superuser and groups

--- LittleLemon/LittleLemonAPI/admin.py
+++ LittleLemon/LittleLemonAPI/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Category, MenuItem, Cart, Order, OrderItem
 
-# Register your models here.
+
+# Customizing the Admin Interface for better usability
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug')
+    search_fields = ('title',)
+    ordering = ('title',)
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'featured', 'category')
+    list_filter = ('category', 'featured')
+    search_fields = ('title', 'category__title')
+    ordering = ('title',)
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'menu_item', 'quantity', 'unit_price', 'total_price')
+    list_filter = ('user',)
+    search_fields = ('user__username', 'menu_item__title')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', 'total', 'date', 'delivery_crew')
+    list_filter = ('status', 'delivery_crew')
+    search_fields = ('user__username', 'delivery_crew__username')
+    ordering = ('-date',)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'menu_item', 'quantity', 'unit_price', 'total_price')
+    list_filter = ('order',)
+    search_fields = ('order__id', 'menu_item__title')
